ecb5b161b6b027b1e826ca41de29041a7cf22a71
Roshan <roxadhi@gmail.com>
2026-05-05 16:08:35 -0600

    #23 get api to see forwarded headers

---
+++ services/userservice/src/main/java/com/myapp/userservice/user/controller/DebugController.java
@@ -0,0 +1,29 @@
+package com.myapp.userservice.user.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/v1/debug")
+@Tag(name = "Users", description = "User management endpoints")
+public class DebugController {
+
+    @GetMapping("/headers")
+    public Map<String, String> headers(HttpServletRequest request) {
+        Map<String, String> result = new HashMap<>();
+        result.put("scheme", request.getScheme());
+        result.put("serverName", request.getServerName());
+        result.put("x-forwarded-proto", request.getHeader("x-forwarded-proto"));
+        result.put("x-forwarded-host", request.getHeader("x-forwarded-host"));
+        result.put("x-forwarded-port", request.getHeader("x-forwarded-port"));
+        return result;
+    }
+
+}
